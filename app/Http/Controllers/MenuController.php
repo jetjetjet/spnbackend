f26625b2c4bd3a4889c6ecfd0eb5bb1931321e07
Agus <agus@summit.com.sg>
2020-04-08 20:01:50 +0700

    .

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\HakAkses;
 use App\Helpers\Helper;
 use App\Model\Menu;
+use App\User;
+use App\Model\Group;
+use App\Model\GroupMenu;
 use Auth;
 use Validator;
 use DB;
@@ -16,6 +19,13 @@ class MenuController extends Controller
   {
     $perm = HakAkses::all();$dw = Auth::user()->getAuthIdentifier();
     return response()->json($perm, 200);
+  }
+
+  public function getMenuSideBar()
+  {
+    $q = Menu::join('gen_groupmenu as gm', 'menu.id', 'menu_id')
+      ->join('gen_group as gg', 'gg.id', 'group_id')
+      ->join('user as u', 'u.id', '');
   }
 
   public function getAll(Request $request)
@@ -32,9 +42,7 @@ class MenuController extends Controller
   public function getById(Request $request, $id = null)
   {
     $result = Helper::$responses;
-    $q = Menu::where('active', '1')
-      ->where('gen_menu.id', $id)
-      ->first();
+    $q = Menu::getById($id)->first();
     if ($q == null){
       $result['state_code'] = 400;
       $result['messages'] = trans('messages.dataNotFound', ["item" => $id]);
@@ -64,58 +72,17 @@ class MenuController extends Controller
       return response()->json($result, 400);
     }
 
-    $result = Helper::$responses;
-    try{
-      $userLogin = Auth::user()->getAuthIdentifier();
-      $menu = null;
-      $mode = "";
-      if ($id){
-        $menu = Menu::where('active', '1')->where('id', $id)->firstOrFail();
-
-        $menu->update([
-          'menu_name' => $inputs['menu_name'],
-          'display' => $inputs['display'],
-          'url' => $inputs['url'],
-          'icon' => $inputs['icon'],
-          'isparent' => $inputs['isparent'],
-          'parent_id' => $inputs['parent_id'],
-          'index' => $inputs['index'],
-          'modified_at' => DB::raw('now()'),
-          'modified_by' => $userLogin
-        ]);
-        $mode = "Ubah";
-      } else {
-        $menu = Menu::create([
-          'menu_name' => $inputs['menu_name'],
-          'display' => $inputs['display'],
-          'url' => $inputs['url'],
-          'icon' => $inputs['icon'],
-          'isparent' => $inputs['isparent'],
-          'parent_id' => $inputs['parent_id'],
-          'index' => $inputs['index'],
-          'active' => '1',
-          'created_at' => DB::raw('now()'),
-          'created_by' => $userLogin
-        ]);
-        $mode = "Simpan";
-      }
-      $result['success'] = true;
-      $result['state_code'] = 200;
-      $result['data'] = $menu;
-      array_push($result['messages'], trans('messages.succesSaveUpdate', ["item" => $menu->menu_name, "item2" => $mode]));
-      return response()->json($result, 200);
-    } catch(\Exception $e){
-      $result['state_code'] = 500;
-      array_push($result['messages'], $e->getMessage());
-      return response()->json($result, 500);
-    }
+    $respon = Helper::$responses;
+    $result = Menu::saveMenu($id, $respon, $inputs, Auth::user()->getAuthIdentifier());
+    return response()->json($result, $result['state_code']);
+    
   }
 
   public function delete(Request $request, $id = null)
   {
     $result = Helper::$responses;
     try{
-      $menu = Menu::where('active', '1')->where('id', $id)->firstOrFail();
+      $menu = Menu::getById($id)->firstOrFail();
 
       $menu->update([
         'active' => '0',
--- app/Model/Menu.php
+++ app/Model/Menu.php
@@ -3,6 +3,7 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Menu extends Model
 {
@@ -21,4 +22,56 @@ class Menu extends Model
     'modified_at',
     'modified_by'
   ];
+
+  public function scopeSaveMenu($query, $id, $result, $inputs, $loginId)
+  {
+    try{
+      $menu = null;
+      $mode = "";
+      if ($id){
+        $menu = $query->where('active', '1')->where('id', $id)->firstOrFail();
+
+        $menu->update([
+          'menu_name' => $inputs['menu_name'],
+          'display' => $inputs['display'],
+          'url' => $inputs['url'],
+          'icon' => $inputs['icon'],
+          'isparent' => $inputs['isparent'],
+          'parent_id' => $inputs['parent_id'],
+          'index' => $inputs['index'],
+          'modified_at' => DB::raw('now()'),
+          'modified_by' => $loginId
+        ]);
+        $mode = "Ubah";
+      } else {
+        $menu = $query->create([
+          'menu_name' => $inputs['menu_name'],
+          'display' => $inputs['display'],
+          'url' => $inputs['url'],
+          'icon' => $inputs['icon'],
+          'isparent' => $inputs['isparent'],
+          'parent_id' => $inputs['parent_id'],
+          'index' => $inputs['index'],
+          'active' => '1',
+          'created_at' => DB::raw('now()'),
+          'created_by' => $loginId
+        ]);
+        $mode = "Simpan";
+      }
+      $result['success'] = true;
+      $result['state_code'] = 200;
+      $result['data'] = $menu;
+      array_push($result['messages'], trans('messages.succesSaveUpdate', ["item" => $menu->menu_name, "item2" => $mode]));
+      return $result;
+    } catch(\Exception $e){
+      $result['state_code'] = 500;
+      array_push($result['messages'], $e->getMessage());
+      return $result;
+    }
+  }
+
+  public function scopeGetById($query, $id)
+  {
+    return $query->where('active', '1')->where('gen_menu.id', $id);
+  }
 }
