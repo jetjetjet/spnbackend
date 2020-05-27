2cc111d562f270273514f04f029ba1e672ec988b
Agus <agus@summit.com.sg>
2020-05-27 17:54:18 +0700

    tambahan sedikit

---
+++ database/migrations/2020_05_20_100730_create_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->boolean('active');
+            $table->timestamp('created_at');
+            $table->integer('created_by');
+            $table->timestamp('modified_at')->nullable();
+            $table->integer('modified_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
---
+++ database/migrations/2020_05_20_101006_create_surat_keluars_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuratKeluarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surat_keluar', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomor_agenda')->nullable();
+            $table->string('nomor_surat')->nullable();
+            $table->dateTime('tgl_surat')->nullable();
+            $table->string('jenis_surat');
+            $table->integer('klasifikasi_surat');
+            $table->string('sifat_surat');
+            $table->string('tujuan_surat');
+            $table->string('hal_surat');
+            $table->string('lampiran_surat');
+            $table->integer('approval_user');
+            $table->integer('file_id')->nullable();
+
+
+            $table->boolean('active');
+            $table->timestamp('created_at');
+            $table->integer('created_by');
+            $table->timestamp('modified_at')->nullable();
+            $table->integer('modified_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surat_keluars');
+    }
+}
