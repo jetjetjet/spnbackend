306ac8428f688178b835981b5c4329339259c797
Afdal <afd.zik@gmail.com>
2020-06-09 01:31:36 +0700

    ubah get

--- routes/api.php
+++ routes/api.php
@@ -21,20 +21,20 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::get('/jabatan/view/{id?}', 'PositionController@getById');
     Route::post('/jabatan/save/{id?}', 'PositionController@save');
     Route::post('/jabatan/delete/{id?}', 'PositionController@delete');
-    Route::post('/jabatan/search', 'PositionController@searchPosition');
+    Route::get('/jabatan/search', 'PositionController@searchPosition');
 
     Route::get('/unit/list', 'GroupController@getAll');
     Route::get('/unit/view/{id?}', 'GroupController@getById');
     Route::post('/unit/save/{id?}', 'GroupController@save');
     Route::post('/unit/delete/{id?}', 'GroupController@delete');
-    Route::post('/unit/search', 'GroupController@searchGroup');
+    Route::get('/unit/search', 'GroupController@searchGroup');
     
     Route::get('/user/list', 'UserController@getAll');
     Route::get('/user/view/{id?}', 'UserController@getById');
     Route::post('/user/save/{id?}', 'UserController@save');
     Route::post('/user/delete/{id?}', 'UserController@delete');
     Route::post('/user/changePassword/{id?}', 'UserController@changePassword');
-    Route::post('/user/search', 'UserController@searchUser');
+    Route::get('/user/search', 'UserController@searchUser');
     Route::post('/user/uploadPhoto/{id}', 'UserController@uploadFoto');
 
     Route::get('/menu/allMenuPermission', 'MenuController@getAllMenuPermission');
